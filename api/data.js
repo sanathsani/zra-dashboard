0b85b62371e699ca6f508a1933db08b75215b269
sanathsani <sanathsani123@gmail.com>
2026-08-14 12:21:36 +0530

    disable vercel cache on api/data

--- api/data.js
+++ api/data.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
     const clean = text.replace(/^\/\*[^*]*\*\/\s*/, '');
     const data = JSON.parse(clean);
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
